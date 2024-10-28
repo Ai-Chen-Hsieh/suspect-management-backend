09e4e684d5e93027caaa3d857bc4e18733c0cc01
Ai-Chen-Hsieh <a0903109727@gmail.com>
2024-10-28 11:10:56 +0800

    testing deploy

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const suspectRouter = require('./modules/suspect');
 
+router.get('/', (req, res) => {
+  return 'hello'
+})
 router.use('/suspect', suspectRouter);
 
 module.exports = router;
